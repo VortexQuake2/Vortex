cdf135c590917748fec261ba8ba5ba1f04793914
zardoru <diao.lzr@gmail.com>
2022-12-15 17:17:32 -0300

    but haste time kept going...

--- src/combat/weapons/p_weapon.c
+++ src/combat/weapons/p_weapon.c
@@ -765,9 +765,10 @@ void Weapon_Generic(
 
     // ent->FrameShot = 0;
 
-    if (!ent->myskills.abilities[HASTE].disable || ent->client->pers.inventory[haste_index]) {
+    if ((!ent->myskills.abilities[HASTE].disable && ent->myskills.abilities[HASTE].current_level >= 1)
+        || ent->client->pers.inventory[haste_index]) {
         // time when to fire next shot
-        float haste_wait = 1;
+        float haste_wait = -1;
         float haste_tech = INFINITY; // "never"
         float haste_skill = INFINITY; // "never"
 
