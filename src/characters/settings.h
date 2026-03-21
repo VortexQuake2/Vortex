182468b2b0b569227e6aa7c43b6c9a9c948af4c4
linear <dev@linear.network>
2026-03-21 17:31:23 -0400

    bump to 5.7.9

--- src/characters/settings.h
+++ src/characters/settings.h
@@ -10,7 +10,7 @@ const char* Time();
 #define	CURRENT_DATE				s1 = Date()
 #define CURRENT_TIME				s2 = Time()
 
-#define VRX_VERSION "5.7.8"
+#define VRX_VERSION "5.7.9"
 
 // autosave interval
 #define AUTOSAVE_FRAMES (300)
