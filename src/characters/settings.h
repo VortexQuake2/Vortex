67ebed9ebbacb33b360eb4e764e1e7c9d53d5f4e
linear cannon <dev@linear.network>
2026-04-23 21:06:07 -0400

    v5.99.5

--- src/characters/settings.h
+++ src/characters/settings.h
@@ -14,7 +14,7 @@ const char* Time();
 #define	CURRENT_DATE				s1 = Date()
 #define CURRENT_TIME				s2 = Time()
 
-#define VRX_VERSION "5.99.4"
+#define VRX_VERSION "5.99.5"
 
 // autosave interval
 #define AUTOSAVE_FRAMES (300)
